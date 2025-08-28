0fe0091f113b442b67e21e440491e6b1daa0d02a
amirulabdlatib <amirulal73@gmail.com>
2025-08-28 11:42:56 +0800

    Install axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,15 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import axios from "axios";
 
-const app = createApp(App)
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router);
+
+app.mount("#app");
