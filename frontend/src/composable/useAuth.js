469ff53bbbbd19bc9d2e64fc5c119f4aea1cba51
amirulabdlatib <amirulal73@gmail.com>
2025-08-28 15:03:47 +0800

    Add simple composable example

---
+++ frontend/src/composable/useAuth.js
@@ -0,0 +1,16 @@
+import { computed, reactive } from "vue";
+
+const state = reactive({
+    name: "Admin",
+});
+
+export default function useAuth() {
+    const getName = computed(() => state.name);
+
+    const setName = (name) => (state.name = name);
+
+    return {
+        getName,
+        setName,
+    };
+}
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,4 +1,6 @@
 <script setup>
+    import Navigation from "@/components/Navigation.vue";
+    import useAuth from "@/composable/useAuth";
     import axios from "axios";
     import { reactive } from "vue";
 
@@ -7,15 +9,19 @@
         password: "",
     });
 
-    const login = async () => {
-        await axios.get("/sanctum/csrf-cookie");
-        const response = await axios.post("/login", form);
+    const { setName } = useAuth();
 
-        console.log(response);
+    const login = async () => {
+        // await axios.get("/sanctum/csrf-cookie");
+        // const response = await axios.post("/login", form);
+        // console.log(response);
+
+        setName("John Doe");
     };
 </script>
 
 <template>
+    <Navigation />
     <form @submit.prevent="login">
         <input type="text" name="email" id="email" placeholder="Email" v-model="form.email" />
         <input type="password" name="password" id="password" placeholder="password" v-model="form.password" />
