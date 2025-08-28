7645df5d7b376b0bfb3b62c5358a77f8bda3fb3e
amirulabdlatib <amirulal73@gmail.com>
2025-08-28 12:13:28 +0800

    Project cleanup

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,23 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+    import axios from "axios";
+    import { reactive } from "vue";
+
+    const form = reactive({
+        email: "",
+        password: "",
+    });
+
+    const login = async () => {
+        const response = axios.post("http://127.0.0.1:8000/login");
+
+        console.log(response);
+    };
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <form @submit.prevent="login">
+        <input type="text" name="email" id="email" placeholder="Email" v-model="form.email" />
+        <input type="password" name="password" id="password" placeholder="password" v-model="form.password" />
+        <button type="submit">Login</button>
+    </form>
 </template>
