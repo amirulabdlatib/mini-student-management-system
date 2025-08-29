a6eab8f873a367ebf00094547413b2121fa4b1e2
amirulabdlatib <amirulal73@gmail.com>
2025-08-29 16:33:43 +0800

    Add login page part 2

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,25 +1,3 @@
-<script setup>
-    import useAuth from "@/composable/useAuth";
-    import { reactive } from "vue";
+<script setup></script>
 
-    const form = reactive({
-        email: "",
-        password: "",
-    });
-
-    const { login: loginAction, errors } = useAuth();
-
-    const login = async () => {
-        loginAction(form);
-    };
-</script>
-
-<template>
-    <form @submit.prevent="login">
-        <input type="text" name="email" id="email" placeholder="Email" v-model="form.email" />
-        <p v-if="errors.email">{{ errors.email[0] }}</p>
-        <input type="password" name="password" id="password" placeholder="password" v-model="form.password" />
-        <p v-if="errors.password">{{ errors.password[0] }}</p>
-        <button type="submit">Login</button>
-    </form>
-</template>
+<template>Home page</template>
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,5 +1,17 @@
 <script setup>
-    //
+    import useAuth from "@/composable/useAuth";
+    import { reactive } from "vue";
+
+    const form = reactive({
+        email: "",
+        password: "",
+    });
+
+    const { login: loginAction, errors } = useAuth();
+
+    const login = async () => {
+        loginAction(form);
+    };
 </script>
 
 <template>
@@ -9,29 +21,35 @@
         </div>
 
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form class="space-y-6">
+            <form class="space-y-6" @submit.prevent="login">
                 <div>
                     <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
                     <div class="mt-2">
                         <input
+                            v-model="form.email"
                             id="email"
                             name="email"
                             type="email"
                             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
-                    <p class="mt-2 text-sm text-red-600" id="email-error">Error</p>
+                    <p class="mt-2 text-sm text-red-600" id="email-error" v-if="errors.email">
+                        {{ errors.email[0] }}
+                    </p>
                 </div>
 
                 <div>
                     <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
                     <div class="mt-2">
                         <input
+                            v-model="form.password"
                             id="password"
                             name="password"
                             type="password"
                             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
-                    <p class="mt-2 text-sm text-red-600" id="email-error">Error</p>
+                    <p class="mt-2 text-sm text-red-600" id="email-error" v-if="errors.password">
+                        {{ errors.password[0] }}
+                    </p>
                 </div>
 
                 <div>
