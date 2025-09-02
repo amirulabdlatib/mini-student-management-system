b6863d8035f853f72d24efef8fe0477c03178370
amirulabdlatib <amirulal73@gmail.com>
2025-09-02 16:09:56 +0800

    Add display all design

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,10 +3,10 @@ import Home from "../views/Home.vue";
 import Dashboard from "@/views/Dashboard.vue";
 import Login from "@/views/Login.vue";
 import Register from "@/views/Register.vue";
-import exampleMiddleware from "@/middleware/exampleMiddleware";
 import middlewarePipeline from "./middlewarePipeline";
 import redirectIfGuest from "@/middleware/redirectIfGuestMiddleware";
 import redirectIfAuthenticated from "@/middleware/redirectIfAuthenticated";
+import Index from "@/views/Student/Index.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +20,14 @@ const router = createRouter({
             path: "/dashboard",
             name: "dashboard",
             component: Dashboard,
+            meta: {
+                middleware: [redirectIfGuest],
+            },
+        },
+        {
+            path: "/students",
+            name: "students.index",
+            component: Index,
             meta: {
                 middleware: [redirectIfGuest],
             },
