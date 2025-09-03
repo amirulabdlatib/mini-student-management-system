cb502a20c4b98924bde047502d683bc85479d3ae
amirulabdlatib <amirulal73@gmail.com>
2025-09-03 15:28:57 +0800

    Add loading spinner on create student page

--- frontend/src/views/Student/Create.vue
+++ frontend/src/views/Student/Create.vue
@@ -1,13 +1,15 @@
 <script setup>
+    import Spinner from "@/components/Spinner.vue";
     import useClass from "@/composable/useClass";
     import useSection from "@/composable/useSection";
     import useStudent from "@/composable/useStudent";
     import router from "@/router";
-    import { onMounted, reactive, watch } from "vue";
+    import { onMounted, reactive, watch, ref } from "vue";
 
     const { fetchClasses, classes } = useClass();
     const { fetchSections, sections } = useSection();
     const { createStudent, errors } = useStudent();
+    const isCreating = ref(false);
 
     const form = reactive({
         name: "",
@@ -17,9 +19,14 @@
     });
 
     const submit = async (form) => {
-        await createStudent(form).then(() => {
-            router.push({ name: "students.index" });
-        });
+        isCreating.value = true;
+        await createStudent(form)
+            .then(() => {
+                router.push({ name: "students.index" });
+            })
+            .finally(() => {
+                isCreating.value = false;
+            });
     };
 
     watch(
@@ -102,9 +109,14 @@
                                 Cancel
                             </a>
                             <button
+                                :disabled="isCreating"
                                 type="submit"
-                                class="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                Save
+                                class="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-indigo-600">
+                                <span v-if="!isCreating">Save</span>
+                                <span v-else class="flex gap-2">
+                                    <p>Saving</p>
+                                    <Spinner />
+                                </span>
                             </button>
                         </div>
                     </div>
